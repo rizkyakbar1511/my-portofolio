65030465e8fb4b9183db61f39f7fa6760d00b635
Muhammad Rizki Akbar <rizki.akbar@it.sigma-tech.co.id>
2024-06-05 20:35:32 +0700

    chore: remove unused next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    dangerouslyAllowSVG: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "skillicons.dev",
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
